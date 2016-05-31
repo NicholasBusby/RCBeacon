dea703ebbfba0c1f190c3f0c99617ea8bb62de19
Nicholas Busby <Nicholas.Busby@Live.com>
2016-05-31 08:47:10 -0400

    bad usings.

--- RCBeacon/WebService/WebService.cs
+++ RCBeacon/WebService/WebService.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Auth;
-using Xamarin.Forms;
 
 namespace WebService
 {
